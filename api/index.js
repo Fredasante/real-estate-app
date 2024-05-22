2c44abe2b5b11eefd1654d26fe000d49e5849692
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-22 15:45:10 +0000

    Created Test API Route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 const app = express();
@@ -13,6 +14,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");

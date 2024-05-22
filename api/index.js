abac86478839957e7441202adaedce48d1287ef5
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-05-22 15:22:27 +0000

    Connected To Database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
